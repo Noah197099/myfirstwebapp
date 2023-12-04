246ddef7525be0df24b2c94714a8475f3d705ad3
Noah <nsaleh8@wgu.edu>
2023-12-04 14:51:44 -0500

    Adds a bunch of new controllers and views. Haven't committed in a while.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -1,25 +1,16 @@
 package com.in28minutes.springboot.myfirstwebapp.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SayHelloController {
-
+	
 	@RequestMapping("say-hello")
-	@ResponseBody
 	public String SayHello(){
-		return "Hello!";
+		return "sayHello";
 	}
-	
-	
-	@RequestMapping("say-hello-html")
-	@ResponseBody
-	public String SayHelloHtml(){
-		StringBuffer sb = new StringBuffer();
-		
-		return "Hello!";
-	}
-	
 }
+
+
---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,25 @@
+package com.in28minutes.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+
+	@RequestMapping(value="login",method = RequestMethod.GET)
+	public String gotoLoginPage() {
+		return "loginPage";
+	}
+	
+	@RequestMapping(value="login",method = RequestMethod.POST)
+	public String gotoWelcomePage(@RequestParam String name,
+			@RequestParam String password, ModelMap model) {
+		model.put("password", password);
+		model.put("name", name);
+		return "welcome";
+	}
+}
