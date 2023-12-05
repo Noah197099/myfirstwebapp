739e6fcc5b19ecd9fc83159924d792464cb8f08f
Noah <nsaleh8@wgu.edu>
2023-12-05 13:47:25 -0500

    Adds new dependencies. Adds TodoController and Todo.html page.

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,16 +1,20 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
-private static List<Todo> todos;
+private static List<Todo> todos = new ArrayList<>();
 static {
 	todos.add(new Todo(1, "in28minutes", "Learn AWS",
 			LocalDate.now().plusYears(1), false));
-	todos.add(new Todo(1, "in28minutes", "Learn DevOps",
+	todos.add(new Todo(2, "in28minutes", "Learn DevOps",
 			LocalDate.now().plusYears(2), false));
-	todos.add(new Todo(1, "in28minutes", "Learn Full Stack Development",
+	todos.add(new Todo(3, "in28minutes", "Learn Full Stack Development",
 			LocalDate.now().plusYears(3), false));
 }
 
